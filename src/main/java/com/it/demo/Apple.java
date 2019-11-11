1d30b6e461cf57c58d1e4fe551711210c55c563a
eclipse_user <eclipse_email@163.com>
2019-11-11 17:59:32 +0800

    test hotfix

--- src/main/java/com/it/demo/Apple.java
+++ src/main/java/com/it/demo/Apple.java
@@ -5,6 +5,7 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("Test Git2.....");
 		System.out.println("Test Git1.....");
+		System.out.println("Test hot_fix...");
 	}
 
 }
